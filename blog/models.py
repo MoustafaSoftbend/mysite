ce33221e4ebda7d999d929af78e38242dd64f4fc
Moustafa <bendib.moustafa@gmail.com>
2019-07-11 15:09:38 +0100

    Added post_list template, made some styling and added options to add posts and images from admin page

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,19 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='logo-image', blank=True)
+    
+    def __str__(self):
+        return "logo-picture"
+    
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text= models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date= models.DateTimeField(blank=True, null=True) 
+    image = models.ImageField(upload_to='profile-image', blank=True)
     
     def __str__(self):
         return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .models import Post, Image
 
 # Create your views here.
+
+def post_list(request):
+    post = Post.objects.filter(published_date__lte=timezone.now()).order_by("-published_date")
+    stuff_for_frontend = {'posts': post,}
+    return render(request,'blog/post_list.html',stuff_for_frontend)
